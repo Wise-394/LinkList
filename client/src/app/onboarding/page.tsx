2d754631fdbb7e97d74f5c18ca595d108f324e3a
wise <114738222+Wise-394@users.noreply.github.com>
2026-07-11 15:21:42 +0800

    feat: enhance OnBoarding component with username input and confirm button

--- client/src/app/onboarding/page.tsx
+++ client/src/app/onboarding/page.tsx
@@ -1,14 +1,34 @@
+"use client";
+import { Button } from "@/components/ui/button";
 export default function OnBoarding() {
   return (
     <div className="flex flex-1 items-center justify-center p-4">
-      <main className="w-full rounded-2xl bg-gray-500 p-4">
+      <main className="w-full max-w-110 rounded-2xl bg-gray-500 p-4">
         <p className="text-sm">Step 1 of 1</p>
         <h1 className="text-2xl font-bold">Enter your LinkList username</h1>
         <p>
           This will become your public profile address. You cant change this
           username later.
         </p>
-        <p className="mt-5 font-bold">USERNAME</p>
+        <div className="mt-5 flex flex-col">
+          <label htmlFor="password" className="mt-5 font-bold">
+            USERNAME
+          </label>
+          <div className="text-1xl flex w-full gap-2 rounded-lg bg-gray-400 p-2">
+            <p>linklist.com/</p>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="enter name"
+              className="flex-1"
+            />
+          </div>
+          <p>this username is available</p>
+          <div className="mt-5">
+            <Button label="confirm" onClick={() => {}} />
+          </div>
+        </div>
       </main>
     </div>
   );
--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -1,20 +1,16 @@
 // components/ui/buttonWithIcon.tsx
 "use client";
-import type { ReactNode } from "react";
-
 interface Props {
-  icon: ReactNode;
   label: string;
   onClick: () => void;
 }
 
-export function ButtonWithIcon({ icon, label, onClick }: Props) {
+export function Button({ label, onClick }: Props) {
   return (
     <button
       onClick={onClick}
       className="flex w-full flex-row items-center justify-center gap-2 rounded-lg bg-white p-3 text-black outline-1 outline-gray-400"
     >
-      {icon}
       <p>{label}</p>
     </button>
   );
