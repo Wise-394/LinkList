40d7909a49cbbc6088a986564484d5ef1a22ee4a
wise <114738222+Wise-394@users.noreply.github.com>
2026-07-16 20:48:15 +0800

    feat: add PhotosCard component for profile and cover image uploads

---
+++ client/src/components/features/forms/photosCard.tsx
@@ -0,0 +1,13 @@
+export function PhotosCard() {
+  <div>
+    <p>Photos</p>
+    <div>
+      <label htmlFor="profileImgInput">Profile Photo </label>
+      <input type="image" id="profileImgInput" name="profileImage" />
+    </div>
+    <div>
+      <label htmlFor="CoverImgInput">Cover Photo </label>
+      <input type="image" id="profileImgInput" name="coverImage" />
+    </div>
+  </div>;
+}
